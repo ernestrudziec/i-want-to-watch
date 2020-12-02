1ed7297f465f98f637c826a88e3aa12e11fd9c4b
Ernest Rudziec <ernest.rudziec@tdy.pl>
2020-12-02 19:26:11 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const cors = require('cors');
+const { parse } = require('path');
 
 let data = fs.readFileSync('data.json');
 let parsedData = JSON.parse(data);
@@ -9,57 +10,51 @@ let parsedData = JSON.parse(data);
 // app.use(express.json({type: '*/*'}));
 app.use(cors());
 
-
 const finished = (err) => {
-    console.log(err);
-}
+	console.log(err);
+};
 
 app.get('/data', (req, res) => {
-    res.send(parsedData);
+	let datas = fs.readFileSync('data.json');
+	let parsedDatas = JSON.parse(datas);
+
+	res.send(parsedDatas);
 });
 
+app.post('/add', express.json({ type: '*/*' }), (req, res) => {
+	req.body.id = parsedData.length + 1;
 
+	res.json(req.body);
 
+	let writeable = JSON.stringify([...parsedData, req.body]);
+	parsedData = JSON.parse(writeable);
 
-app.post('/add', express.json({type: '*/*'}), (req, res) => {
-
-    req.body.id = parsedData.length+1;
-
-
-    res.json(req.body);
-
-    let writeable = JSON.stringify([...parsedData, req.body]);
-    parsedData = JSON.parse(writeable);
-
-    fs.writeFile('data.json', writeable, finished );
-
-
+	fs.writeFile('data.json', writeable, finished);
 });
 
 app.put('/change/:id', (req, res) => {
+	let putObjectIndex = parsedData.findIndex((x) => x.id == req.params.id);
 
-    let putObjectIndex = parsedData.findIndex(x => x.id == req.params.id);
+	parsedData[putObjectIndex].isWatched = req.body.isWatched;
 
-    parsedData[putObjectIndex].isWatched = req.body.isWatched;
-
-    let writeable = JSON.stringify([...parsedData, req.body]);
-    parsedData = JSON.parse(writeable);
-    res.send(writeable);
-    fs.writeFile('data.json', writeable, finished );
-
+	let writeable = JSON.stringify([...parsedData, req.body]);
+	parsedData = JSON.parse(writeable);
+	res.send(writeable);
+	fs.writeFile('data.json', writeable, finished);
 });
 
 app.delete('/delete/:id', (req, res) => {
-
-    parsedData.splice(req.params.id, 1);
-    let writeable = JSON.stringify(parsedData);
-    res.send(parsedData);
-    fs.writeFile('data.json', writeable, finished );
-
+	parsedData.splice(req.params.id, 1);
+	let writeable = JSON.stringify(parsedData);
+	res.send(parsedData);
+	fs.writeFile('data.json', writeable, finished);
 });
 
-
-
+app.post('/reset', (req, res) => {
+	let writeable = JSON.stringify([]);
+	res.send(writeable);
+	fs.writeFile('data.json', writeable, finished);
+});
 
 const port = process.env.PORT || 8080;
 
